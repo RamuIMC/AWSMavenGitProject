86677ff83b89fee6a946881c646171a48d1cad13
Ramakrishna <mramakrishna.mrk@gmail.com>
2016-08-25 23:00:16 +0530

    Filters Implementation

--- src/main/webapp/resources/scripts/controller/GetAllBooksController.js
+++ src/main/webapp/resources/scripts/controller/GetAllBooksController.js
@@ -8,4 +8,24 @@ app.controller('GetAllBooksController',['$scope','allbooks',function($scope,allb
 	// });
 	
 	$scope.title = "AngularJS Application";
+	$scope.priceRange = [0, 6000];
+	
+	$scope.filterPrice=function(book){
+		if(book.price > $scope.priceRange[0] && book.price < $scope.priceRange[1]){
+			return true;
+		}
+		
+	};
+	$scope.orderByPrice=function(book){
+		if($scope.orderBy == "price-low-high"){
+			
+			return book.price;
+		}
+		if($scope.orderBy == "price-high-low"){
+			
+			return -book.price;
+		}
+		else return book.price;
+		
+	};
 }]);
